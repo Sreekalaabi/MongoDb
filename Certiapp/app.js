a57045c21f44dccaf83d5f6b34a64d6ef5c5f3a4
Sreekalaabi <sreekalajanardhanan@gmail.com>
2024-07-23 13:56:27 +0530

    modified

--- Certiapp/app.js
+++ Certiapp/app.js
@@ -1,16 +1,21 @@
 const routes =require("./routes.js")
 const express =require('express');
 const {mongoose} = require ('mongoose');
+const dotenv=require('dotenv');
+dotenv.config();
+const uri= process.env.mongo_uri;
 
 const app =express();
-const PORT =3008;
+const PORT =3005;
 app.listen(PORT,()=>{
 console.log(`Server is running on the port ${PORT}`);
 });
 app.use(express.json());
 app.use('/',routes);
 
-mongoose.connect("mongodb://localhost:27017/Certi")
+mongoose.connect(uri
+
+);
 
 const database =mongoose.connection;
 database.on("error",(error)=>{
